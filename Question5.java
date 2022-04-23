7af2cff442761ccd73bf38edafd57cca800a3da1
Gabriel-Koh <s10219353@connect.np.edu.sg>
2022-04-24 04:24:32 +0800

    Exercise 1

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Question5
 {
@@ -27,6 +28,46 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    int len = in.nextInt();
+    int[] list = new int[len];
+    for (int i = 0;
+    i < len;
+    i++){
+      int temp = in.nextInt();
+      list[i] = temp;
+    }
     
+    Arrays.sort(list);
+    
+    int count1 = 0;
+    int count2 = 0;
+    int tempvar =0;
+    int mode =0;
+    
+    for (int x = 0;
+     x < len; 
+     x++){
+            tempvar = list[x];
+            count1 = 1;
+        
+        for (int y = x + 1; 
+        y < len; 
+        y++){
+            if (tempvar == list[y]) count1++;
+        }
+        
+        if (count1 > count2)
+        {
+                mode = tempvar;
+                count2 = count1;
+        }
+        
+        else if(count1 == count2)
+        {
+            mode = Math.min(mode, tempvar);
+        }
+    }
+    System.out.println(mode);
+    in.close();
   }
 }
